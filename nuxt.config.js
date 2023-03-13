e825772a23fc0e41d813d3e2416e301bb121ba35
= <pezi10002@hotmail.com>
2023-03-13 15:55:52 +0100

    added stringsanitizer, icon & limited tasktitle-maxlength

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,8 +53,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8000',
-    // baseURL: 'https://todoapi.peterstrele.com',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://todoapi.peterstrele.com',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
